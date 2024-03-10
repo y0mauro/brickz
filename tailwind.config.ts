4a0de22bc12dde5b43b6b4fd91ca3fda9b6a8624
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-03-10 18:29:35 +0100

    Remove unused import and plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
-import type { Config } from "tailwindcss"
-import animations from '@midudev/tailwind-animations'
+import type { Config } from "tailwindcss";
 
 
 const config = {
@@ -76,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate") , animations],
+  plugins: [require("tailwindcss-animate") , ],
 } satisfies Config
 
 export default config
